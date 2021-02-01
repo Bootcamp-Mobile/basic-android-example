f31f9b7987ea1c98d37867b1879616aeeafbaa40
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-02-01 17:45:27 +0100

    Clase 01/02/2021

--- app/src/main/java/com/cbellmont/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/cbellmont/miprimeraapp/MainActivity.kt
@@ -3,11 +3,20 @@ package com.cbellmont.miprimeraapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mi_segunda_pantalla)
+        setContentView(R.layout.mi_pantalla_radio)
+        /* DESCOMENTAR PARA PROBAR mi_scroll_view
+        val textView = findViewById<TextView>(R.id.textoHolas)
+        var texto = "Empezamossss....\n"
+        for (i in 0..100) {
+            texto += "Hola $i :) \n"
+        }
+        textView.text = texto
+        */
         Log.d("CARLOS", "Hola Hola, estoy en el onCreate")
     }
 }
